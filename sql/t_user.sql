c04a499df3cb998dcade4525390c2d7b75b2080a
zhangyufeng0123 <1691903105@qq.com>
2024-02-02 10:25:46 +0800

    update

--- sql/t_user.sql
+++ sql/t_user.sql
@@ -3,7 +3,7 @@ create table t_user
     `id`              BIGINT(20) not null comment '用户ID，手机号码',
     `nickname`        VARCHAR(255) not null,
     `password`        VARCHAR(32)  default null comment 'MDS(MDS(pass明文+固定salt)+salt)',
-    `slat`            VARCHAR(10)  default null,
+    `salt`            VARCHAR(10)  default null,
     `head`            VARCHAR(128) default null comment '头像',
     `register_date`   datetime null comment '注册时间',
     `last_login_date` datetime     default null comment '最后一次登录时间',
--- src/main/java/org/gotomove/flashsale/controller/UserController.java
+++ src/main/java/org/gotomove/flashsale/controller/UserController.java
@@ -1,8 +1,13 @@
 package org.gotomove.flashsale.controller;
 
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.gotomove.flashsale.pojo.User;
+import org.gotomove.flashsale.vo.RespBean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * <p>
@@ -14,6 +19,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 @RequestMapping("/user")
+@Api(value = "用户表", tags = "用户表")
 public class UserController {
-
+    @RequestMapping("/info")
+    @ResponseBody
+    @ApiOperation("返回用户信息")
+    public RespBean info(User user) {
+        return RespBean.success(user);
+    }
 }
